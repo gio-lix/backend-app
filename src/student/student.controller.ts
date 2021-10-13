b6d724fefca2adc36b7b4fe91674a598ffbd5492
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2021-10-13 20:35:53 +0400

    add more coontrallers

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import {Controller, Get, Post, Put} from '@nestjs/common'
 
 @Controller('students')
 export class studentController {
@@ -6,5 +6,20 @@ export class studentController {
     @Get()
     getStudents() {
         return 'all students'
+    }
+
+    @Get('/:studentId')
+    getStudentById() {
+        return 'get student by id'
+    }
+
+    @Post()
+    creatStudent() {
+        return 'Create student'
+    }
+
+    @Put('/:studentId')
+    updateStudent() {
+        return 'Update student By Id'
     }
 }
